8dc9a49e4e1fde2ce6386bbe461abd6d20785bcd
xuchao <pioneer_chao@foxmail.com>
2020-03-05 20:30:50 +0800

    [189] 旋转数组

---
+++ 189.旋转数组.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=java
+ *
+ * [189] 旋转数组
+ */
+
+// @lc code=start
+class Solution {
+    public void rotate(int[] nums, int k) {
+
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+
+    public void reverse(int[] nums, int start, int end){
+        while(start < end){
+            int t = nums[start];
+            nums[start] = nums[end];
+            nums[end] = t;
+            start++;
+            end--;
+        }
+    }
+
+}
+// @lc code=end
+
