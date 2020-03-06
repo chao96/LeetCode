3690680f01723e19ee1b26788fd7123002af3ff7
xuchao <pioneer_chao@foxmail.com>
2020-03-06 11:18:08 +0800

    [49] 字母异位词分组

---
+++ 49.字母异位词分组.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=49 lang=java
+ *
+ * [49] 字母异位词分组
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs.length == 0){
+            return null;
+        }
+        Map<String, List<String>> map = new HashMap<>();
+        for(int i = 0; i < strs.length; i++){
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String s = String.valueOf(chars);
+
+            if(!map.containsKey(s)){
+                map.put(s, new ArrayList<>());
+            }
+            map.get(s).add(strs[i]);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
+// @lc code=end
+
