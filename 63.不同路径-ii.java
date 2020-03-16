7677c005be78115f56027b4a350dfe5cad3d556b
xuchao <pioneer_chao@foxmail.com>
2020-03-16 11:14:37 +0800

    [63] 不同路径 II

--- 63.不同路径-ii.java
+++ 63.不同路径-ii.java
@@ -7,7 +7,20 @@
 // @lc code=start
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        for(int i = 0; i < n; i++) obstacleGrid[0][i] = 1;
+        for(int i = 0; i < m; i++) obstacleGrid[i][0] = 1;
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(obstacleGrid[i][j] == 1){
+                    obstacleGrid[i][j] = 0;
+                } else {
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+                }
+            }
+        }
+        return obstacleGrid[m-1][n-1];
     }
 }
 // @lc code=end
